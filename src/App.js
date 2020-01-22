2aaae81f0ce1f361d0528c928fd688b12e337476
Hester Knight <hesterknight@hesters-mbp.mynet>
2020-01-22 14:29:12 +0000

    created components

--- src/App.js
+++ src/App.js
@@ -5,8 +5,8 @@ function App() {
   return (
     <div className="App">
       <p>
-        Read through the instructions in the README.md file to build your NASA
-        app! Have fun 🚀!
+        Hester's NASA
+        app 🚀
       </p>
     </div>
   );
